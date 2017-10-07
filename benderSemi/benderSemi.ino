4df0c3613522ec53b59f24d119008e41354b1730
Sociopath <bartek102842@gmail.com>
2017-10-07 19:28:43 +0200

    dodajemy podstawe praktycznie czysty plik

---
+++ benderSemi/benderSemi.ino
@@ -0,0 +1,28 @@
+
+
+//wyswietlacz zmienne
+#include <LiquidCrystal.h>
+#include <EEPROM.h>
+#define btnRIGHT  0
+#define btnUP     1
+#define btnDOWN   2
+#define btnLEFT   3
+#define btnSELECT 4
+#define btnNONE   5 
+
+
+
+
+
+//-------
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
