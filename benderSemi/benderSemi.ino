0c7bc4f223857379e614964854772e40b697fb37
Sociopath <bartek102842@gmail.com>
2017-10-07 19:38:13 +0200

    import bliblioteki lcd i stwozenie obiektu lcd

--- benderSemi/benderSemi.ino
+++ benderSemi/benderSemi.ino
@@ -1,8 +1,10 @@
 
 
-//wyswietlacz zmienne
 #include <LiquidCrystal.h>
 #include <EEPROM.h>
+
+
+//wyswietlacz zmienne
 #define btnRIGHT  0
 #define btnUP     1
 #define btnDOWN   2
@@ -10,19 +12,51 @@
 #define btnSELECT 4
 #define btnNONE   5 
 
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
+int lcd_key  = 0;
+int adc_key_in  = 0;
 
 
+//obsluga lcd klawiszy 
 
+int read_LCD_buttons()
+{
+ adc_key_in = analogRead(0);      // read the value from the sensor 
+ // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
+ // we add approx 50 to those values and check to see if we are close
+ if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
+ // For V1.1 us this threshold
+ if (adc_key_in < 50)   return btnRIGHT;  
+ if (adc_key_in < 250)  return btnUP; 
+ if (adc_key_in < 450)  return btnDOWN; 
+ if (adc_key_in < 650)  return btnLEFT; 
+ if (adc_key_in < 850)  return btnSELECT;  
 
-//-------
+ // For V1.0 comment the other threshold and use the one below:
+/*
+ if (adc_key_in < 50)   return btnRIGHT;  
+ if (adc_key_in < 195)  return btnUP; 
+ if (adc_key_in < 380)  return btnDOWN; 
+ if (adc_key_in < 555)  return btnLEFT; 
+ if (adc_key_in < 790)  return btnSELECT;   
+*/
 
+ return btnNONE;  // when all others fail, return this...
+}
 
-void setup() {
-  // put your setup code here, to run once:
+// ============= 
+
+void setup() { 
+
+  lcd.begin(16,2);
+  lcd.setCursor(0,0);
+
+  
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ 
 
 }
